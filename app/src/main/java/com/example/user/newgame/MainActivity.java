67792db7b1f99e2617349e524741d6c2ae03c048
oriisking <oriisking@gmail.com>
2017-07-07 13:39:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/newgame/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.newgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void checkIfCorrect()
+    {
+        int i = 1+1;
+    }
+}
