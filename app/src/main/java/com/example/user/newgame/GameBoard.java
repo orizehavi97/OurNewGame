b2549f06150952636093b7aa2758641beb2023ab
oriisking <oriisking@gmail.com>
2017-07-07 18:59:55 +0300

    Added hp data member to Character.java

---
+++ app/src/main/java/com/example/user/newgame/GameBoard.java
@@ -0,0 +1,19 @@
+package com.example.user.newgame;
+
+/**
+ * Created by user on 07/07/2017.
+ */
+
+public class GameBoard {
+
+    //Data members
+    private Point[][] boardMatrix;
+
+    /**
+     * Building a new instance of a board object.
+     */
+    public GameBoard()
+    {
+        boardMatrix = new Point[10][10];
+    }
+}
